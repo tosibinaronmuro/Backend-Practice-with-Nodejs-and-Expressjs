5827162b0c36937ca4d859148902030eccb54d68
Tosibinaronmuro <tosironj@gmail.com>
2023-01-04 21:16:05 +0100

    created a json file and an API

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
  const express=require('express')
  const app=express()
- const path=require('path')
-
-//  setup static resources and middleware
- app.use(express.static('./frontend'))
-
+const {randomData}=require('./data')
 
  app.get('/',(req,res)=>{
-res.sendFile(path.join(__dirname,'./frontend/index.html'))
+    res.status(200).send('<h4>hellooo there and welcome to our homepage click </h4><a href="./api/products">here</a>')
+ 
+ })
+ app.get('/api/products',(req,res)=>{
+    const users=randomData.map((user)=>{
+         
+        
+        return user.users.map((person)=>{
+            const {name,work,email,dob}=person;
+            return {name,work,email,dob}
+        })
+    })
+    res.status(200).send(users)
+
+    // res.status(200).send(randomData)
+ })
+app.all('*',(req,res)=>{
+    res.status(404).send('<h4>page not found... click  </h4><a href="/">here</a>')
+})
+
+
+ app.listen(3300, ()=>{
+    console.log('listening to the app on port 3300');
  })
 
-app.all('*',(req,res)=>{
-    res.status(404).send('resource not found')
-})
- app.listen(3300,()=>{
-    console.log('listening to the server on port 3300')
- })
---
+++ data.js
@@ -0,0 +1,72 @@
+ const randomData=[
+    {   
+        "users": [
+          {
+            "id": 0,
+            "name": "Adam Carter",
+            "work": "Unilogic",
+            "email": "adam.carter@unilogic.com",
+            "dob": "1978",
+            "address": "83 Warner Street",
+            "city": "Boston",
+            "optedin": true
+          },
+          {
+            "id": 1,
+            "name": "Leanne Brier",
+            "work": "Connic",
+            "email": "leanne.brier@connic.org",
+            "dob": "1987",
+            "address": "9 Coleman Avenue",
+            "city": "Toronto",
+            "optedin": false
+          }
+        ],
+        "images": [
+          "img0.png",
+          "img1.png",
+          "img2.png"
+        ],
+        "coordinates": {
+          "x": 35.12,
+          "y": -21.49
+        },
+        "price": "$159,395"
+      },
+      { 
+        "users": [
+          {
+            "id": 1,
+            "name": "Adam john",
+            "work": "twitter",
+            "email": "adam.john@twitter.com",
+            "dob": "1945",
+            "address": "83 Warner house",
+            "city": "milpitas",
+            "optedin": false
+          },
+          {
+            "id": 2,
+            "name": "Leanne Brier",
+            "work": "Connic",
+            "email": "leanne.brier@connic.org",
+            "dob": "1987",
+            "address": "9 Coleman Avenue",
+            "city": "Toronto",
+            "optedin": false
+          }
+        ],
+        "images": [
+          "img0.png",
+          "img1.png",
+          "img2.png"
+        ],
+        "coordinates": {
+          "x": 35.12,
+          "y": -21.49
+        },
+        "price": "$59,395"
+      }
+]
+
+module.exports={randomData}
